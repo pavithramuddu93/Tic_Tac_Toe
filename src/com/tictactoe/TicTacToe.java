f1dc020e1d4582b55649cf254d5d2beb441484d0
pavithramuddu93 <pavithramuddu93>
2022-05-19 18:25:42 +0530

    UC5 added

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -3,54 +3,67 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    public static Scanner input = new Scanner(System.in);
-    public static char choice;
-    public static int size = 10;
-    public static int placeSelect;
-    public static char[] board = new char[size];
+    public static char inputUser;
+    public static char inputC;
+    public static char[] board = new char[10];
 
-    public static void initGame() {
-        System.out.println();
-        for (int index = 1; index < size; ++index) {
-            board[index] = ' ';  // all cells empty
-            System.out.println("Value of index " + index + " is " + board[index]);
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+        inputUser = input();
+        if (inputUser == 'X') {
+            inputC = 'O';
+        } else {
+            inputC = 'X';
+        }
+        TicTacToeGame.showBoard();
+        TicTacToeGame.uSelection();
+        TicTacToeGame.showBoard();
+        TicTacToeGame.cSelection();
+        TicTacToeGame.showBoard();
+    }
+
+    public static char input() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your input in X / O");
+        char inputU = in.next().charAt(0);
+        return inputU;
+    }
+
+    public static void showBoard() {
+        System.out.println("TicTacToe Board");
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("---------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("---------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+    }
+
+    public static void uSelection() {
+        int j = 0;
+        while (j < 1) {
+            System.out.println("Please enter the index from 1 to 9 to make the move");
+            Scanner in1 = new Scanner(System.in);
+            int select = in1.nextInt();
+            if (board[select] == ' ') {
+                board[select] = inputUser;
+                j = 1;
+            } else {
+                System.out.println("Seat Occupied, please enter a different input");
+            }
         }
     }
-    public static void selection() {
-        System.out.println("\nWould you like to choose 'x' or 'o' to play");
-        choice = input.next().charAt(0);
-        System.out.println("Choice is" +choice);
-    }
-    private static void printBoard() {
-        System.out.println("-------");
-        System.out.println("|" + board[1] + "|" + board[2] + "|" + board[3] + "|");
-        System.out.println("-------");
-        System.out.println("|" + board[4] + "|" + board[5] + "|" + board[6] + "|");
-        System.out.println("-------");
-        System.out.println("|" + board[7] + "|" + board[8] + "|" + board[9] + "|");
-        System.out.println("-------");
-    }
-    public static boolean indexCheck(char[] board, int PlaceSelect) {
-        boolean result = (board[PlaceSelect] == ' ');
-        System.out.println("Result of boolean check is: " +result);
-        return result;
-    }
 
-    public static void makingMove(char[] board, int PlaceSelect, char Choice) {
-        board[PlaceSelect] = Choice;
-        printBoard();
-    }
-    public static void main(String[] args) {
-        initGame();
-        selection();
-        printBoard();
-
-        System.out.println("Select the position where you want to move");
-        placeSelect = input.nextInt();
-        if (indexCheck(board, placeSelect)) {
-            makingMove(board, placeSelect, choice);
-        } else {
-            System.out.println("Hello computer " + placeSelect + " is not a valid move.");
+    public static void cSelection() {
+        int i = 0;
+        while (i < 1) {
+            int select = (int) ((Math.floor(Math.random() * 10) % 8) + 1);
+            if (board[select] == ' ') {
+                board[select] = inputC;
+                i = 1;
+            }
         }
     }
 }
